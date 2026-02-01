5a2e9cee4b3ac435973f6b283209aaf8967294c8
hmouraf <humbertomoura@gmail.com>
2026-02-01 20:58:43 -0300

    delete

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "meu-eks-terraform-state"
-    key            = "fiapx-infra-roles/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "meu-eks-terraform-lock-001"
-    encrypt        = true
-  }
-}
