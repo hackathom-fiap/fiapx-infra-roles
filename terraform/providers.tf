11a45c1406a2550e3c7e185124ee2c50520b44ec
hmouraf <humbertomoura@gmail.com>
2026-02-01 21:12:47 -0300

    feat role

---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Por favor, altere para a sua região de preferência
+  region = "us-east-1"
+}
